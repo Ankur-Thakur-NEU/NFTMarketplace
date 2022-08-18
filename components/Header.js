3c3a317f7ae26d4007979c28a3842e028a43d440
Ankur Thakur <ankurthakur.neu@gmail.com>
2022-08-18 03:45:00 -0400

    fixes

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,14 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 flex flex-row justify-between items-center bg-indigo-600">
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
             <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
             <div className="flex flex-row items-center">
                 <Link href="/">
-                    <a className="mr-4 p-6">Home</a>
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                 </Link>
                 <Link href="/sell-nft">
-                    <a className="mr-4 p-6">Sell NFT</a>
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sell NFT</a>
                 </Link>
                 <ConnectButton moralisAuth={false} />
             </div>
